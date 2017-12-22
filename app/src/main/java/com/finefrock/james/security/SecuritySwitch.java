f2297bdf1a1d8354e4e6e5b4c8cedda34a392ef1
James Finefrock <finefrockjames@gmail.com>
2017-12-22 16:22:06 -0500

    Updated SecuritySwitch class to include information from Firebase about last opened time

--- app/src/main/java/com/finefrock/james/security/SecuritySwitch.java
+++ app/src/main/java/com/finefrock/james/security/SecuritySwitch.java
@@ -1,6 +1,8 @@
 package com.finefrock.james.security;
 
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 
 /**
  * Created by james on 12/17/17.
@@ -17,19 +19,25 @@ public class SecuritySwitch {
     public int id;
     public String name;
     public boolean open;
+    public int last_opened;
 
     private SecuritySwitch() {
 
     }
 
-    public SecuritySwitch(long id, String name, Boolean open) {
+    public SecuritySwitch(long id, String name, Boolean open, int last_opened) {
         this.id = (int) id;
         this.name = name;
         this.open = open;
+        this.last_opened = last_opened;
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + "\nOpen: " + open;
+        long milliseconds = Long.valueOf(last_opened + 18000)*1000;// its need to be in milisecond
+        Date date = new java.util.Date(milliseconds);
+        String dateString = new SimpleDateFormat("hh:mma MM dd, yyyy ").format(milliseconds);
+
+        return "Name: " + name + "\nOpen: " + open + "\nLast Opened: " + dateString;
     }
 }
