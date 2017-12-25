3b6cd22551293e9906831886b1de65c7c6839f6e
James Finefrock <finefrockjames@gmail.com>
2017-12-24 20:49:56 -0500

    Moved layout into scrollview. Improved consistency with space between time and AM/PM

--- app/src/main/java/com/finefrock/james/security/SecuritySwitch.java
+++ app/src/main/java/com/finefrock/james/security/SecuritySwitch.java
@@ -36,7 +36,7 @@ public class SecuritySwitch {
     public String toString() {
         long milliseconds = Long.valueOf(last_opened + 18000)*1000;// its need to be in milisecond
         Date date = new java.util.Date(milliseconds);
-        String dateString = new SimpleDateFormat("hh:mma MM dd, yyyy ").format(milliseconds);
+        String dateString = new SimpleDateFormat("hh:mm a MM dd, yyyy ").format(milliseconds);
 
         return "Name: " + name + "\nOpen: " + open + "\nLast Opened: " + dateString;
     }
